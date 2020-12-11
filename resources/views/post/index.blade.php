7962aacac0f69f6c2843c0339cb1d31085ecce35
EC2 Default User <ec2-user@ip-172-31-25-42.ap-northeast-1.compute.internal>
2020-12-11 11:23:51 +0000

    ナビゲーションバーを実装 #3

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,5 +1,11 @@
+@extends('layouts.app')
+@include('navbar')
+@section('content')
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
 <p>このページは仮のページです。</p>
 
 <a href="%" class="btn btn-primary">仮のボタンです</a>
+
+@endsection
