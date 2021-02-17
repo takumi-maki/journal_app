1e841f4446264c1519eaeb2f3fe497f233d2d654
EC2 Default User <ec2-user@ip-172-31-25-42.ap-northeast-1.compute.internal>
2021-02-17 13:28:59 +0000

    initial commit

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -19,13 +19,13 @@
                 
                 
                 @if ($post->post_comment)
-                <h6 class="post_comment">{{$post->post_comment }}</h6>
+                <h6 class="post_comment">{{ $post->post_comment }}</h6>
                 @endif
                 
                 <div class="profile-wrap mt-4">
                     <a class="no-text-decoration" href="/users/{{ $post->user->id }}">
                         @if ($post->user->profile_photo)
-                        <img class="post-profile-icon round-img mr-4" src="{{ secure_asset( $post->user->profile_photo }}" />
+                        <img class="post-profile-icon round-img mr-4" src="{{ $post->user->profile_photo }}" />
                         @else 
                         <img class="post-profile-icon round-img mr-4" src="{{ secure_asset('/images/blank_profile.png') }}" />
                         @endif
